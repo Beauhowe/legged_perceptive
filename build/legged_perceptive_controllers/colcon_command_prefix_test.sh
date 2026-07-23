fceae3737682bd6258971f4a2d9cab62c0919c38
Beauhowe <zbohao7@gmail.com>
2026-07-23 06:40:38 +0000

    [feat] 添加gitingore 删除编译中间量

--- build/legged_perceptive_controllers/colcon_command_prefix_test.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/workspace/src/legged_perceptive/install/legged_perceptive_interface/share/legged_perceptive_interface/package.sh"
-. "/workspace/src/legged_perceptive/install/legged_perceptive_controllers/share/legged_perceptive_controllers/package.sh"
--- build/perceptive_sanitize/CMakeFiles/legged_perceptive_interface.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "liblegged_perceptive_interface.a"
-)
